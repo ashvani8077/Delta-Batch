e2e5f5328bd3d485e4e9981fb47a7db23e4167c0
Ashvani Sharma <panditashuabvp@gmail.com>
2024-12-30 10:00:00 +0530

    OOPs in JavaScript - 30 December 2024

---
+++ Day-33 JS Miscellaneous/oops/app.js
@@ -0,0 +1,24 @@
+const stu1={
+    name:'adam',
+    age:23,
+    marks:43,
+    getMarks:function(){
+        return this.marks;
+    }
+};
+const stu2={
+    name:'bob',
+    age:12,
+    marks:13,
+    getMarks:function(){
+        return this.marks;
+    }
+};
+const stu3={
+    name:'alies',
+    age:33,
+    marks:41,
+    getMarks:function(){
+        return this.marks;
+    }
+};

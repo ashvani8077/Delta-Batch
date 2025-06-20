d35d3f79ac38d69f29d209085fd930919904495d
Ashvani Sharma <panditashuabvp@gmail.com>
2025-06-20 16:21:04 +0530

    Day 30 math in js

---
+++ Day-30 Nodejs/math.js
@@ -0,0 +1,21 @@
+module.exports.sum=(a,b)=>a+b;
+const mul=(a,b)=>a*b;
+const g=9.8;
+const pi=3.14;
+
+// module.exports="Hello";
+// let obj={
+//     sum:sum,
+//     mul:mul,
+//     g:g,
+//     pi:pi
+// }
+// module.exports=obj;
+
+module.exports={
+    sum:sum,
+    pi:pi,
+    mul:mul,
+    g:g,
+    pi:pi,
+}
---
+++ Day-30 Nodejs/script.js
@@ -0,0 +1,12 @@
+// let n=5;
+
+// for(let i=0;i<n;i++){
+//     console.log('Hello, ',i);
+// }
+// let args=process.argv;
+// for(let i=2;i<args.length;i++){
+//     console.log("Hello and welcome to ",args[i]);
+// }
+const someValue=require("./math");
+console.log(someValue);
+console.log(someValue.sum(9,4));
